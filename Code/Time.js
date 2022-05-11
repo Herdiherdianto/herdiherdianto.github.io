11e852143741fed6595d4878b8de72ac18a0970a
HerdiHerdianto <herdiherdianto064@gmail.com>
2022-05-11 19:48:30 +0700

    origin master

--- Code/Time.js
+++ Code/Time.js
@@ -1,5 +1,11 @@
 var time = new Date();
-var day = time.getDay();
-document.getElementById("Tanggal").innerHTML = time.toLocaleDateString();
-document.getElementById("Jam").innerHTML = time.toLocaleTimeString();
-var dayarray = new Array("minggu,", "Senin,", "Selasa,", "Rabu,", "Kamis,", "Jumat,", "Sabtu,");
+let months = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"];
+let myDays = ["Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jum&#39;at", "Sabtu"];
+let date = new Date();
+let day = date.getDate();
+let month = date.getMonth();
+let thisDay = date.getDay();
+thisDay = myDays[thisDay];
+let yy = date.getFullYear();
+let year = yy < 1000 ? yy + 1900 : yy;
+document.getElementById("Tanggal").innerHTML = thisDay + "," + day + " " + months[month] + " " + year;
